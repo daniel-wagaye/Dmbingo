c6fd159e5f2d98819c7195eb29c2f119ef1d8c36
Daniel Wagaye <daniel-wagaye@users.noreply.github.com>
2026-04-21 17:18:04 +0300

    adjust toast container top offset from 35px to 45px to prevent overlap with header

--- game-client/src/App.tsx
+++ game-client/src/App.tsx
@@ -31,7 +31,7 @@ function AppRoutes() {
       <Toaster
         position="top-center"
         containerStyle={{
-          top: 'calc(env(safe-area-inset-top, 0px) + 35px)',
+          top: 'calc(env(safe-area-inset-top, 0px) + 45px)',
         }}
         toastOptions={{
           style: {
